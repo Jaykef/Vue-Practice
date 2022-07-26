89d15616f4acba551ab4c9b34abbfd0081f67fb1
Jaward Sesay <jkef96@icloud.com>
2022-07-26 14:10:36 +0800

    Add files via upload

---
+++ Vue-3-Cart/components/ReviewList.js
@@ -0,0 +1,22 @@
+app.component('review-list', {
+    prop: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    `<div class="review-container">
+    <h3 v-if="!reviews.length">There are no reviews yet.</h3>
+    <div v-else>
+    <h3>Reviews:</h3>
+      <ul>
+        <li v-for="(review, index) in reviews" :key="index">
+          {{ review.name }} gave this {{ review.rating }} stars
+          <br/>
+          "{{ review.text }}"
+        </li>
+      </ul>
+    </div>
+  </div>`
+})
